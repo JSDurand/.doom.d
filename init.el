0504b5931a07508492f87423612c5f4e4aa39b34
Durand <mmemmew@gmail.com>
2020-07-07 02:46:21 +0800

    Go back to lsp.

--- init.el
+++ init.el
@@ -106,7 +106,8 @@
         +dictionary             ; a dictionary in emacs is good.
         ;; +docsets
         )               ; ...or in Dash docsets locally
-       (lsp +eglot)
+       (lsp ;; +eglot
+            )
        ;;macos             ; MacOS-specific commands
        magit                    ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
